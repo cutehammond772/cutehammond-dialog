bf055c322bb1af4f4dea74b47acb23063d1f6c75
LEE\JHL <cutehammond772@gmail.com>
2023-03-22 17:48:23 +0900

    fix(config): Rollup 번들링 오류 수정
    (cherry picked from commit bafb3c36f184c9e2b963251750f1207a11c7462f)

--- rollup.config.js
+++ rollup.config.js
@@ -5,41 +5,57 @@ import peerDeepsExternal from "rollup-plugin-peer-deps-external";
 import json from "@rollup/plugin-json";
 import terser from "@rollup/plugin-terser";
 import typescript from "rollup-plugin-typescript2";
+import dts from "rollup-plugin-dts";
+import ttypescript from "ttypescript";
 
 process.env.BABEL_ENV = "production";
 
 // 빌드 시 빌드 대상이 되는 파일의 확장자입니다.
-const EXTENSIONS = [".ts", ".tsx"];
+const EXTENSIONS = [".js", ".jsx", ".ts", ".tsx"];
 
 // 빌드 옵션을 나타냅니다.
-const ESM_OPTION = { type: "esm", dir: "esm", extension: "js" };
-const CJS_OPTION = { type: "cjs", dir: "cjs", extension: "cjs" };
+const ESM_OPTION = { type: "esm", extension: "js", decl: "ts" };
+const CJS_OPTION = { type: "cjs", extension: "cjs", decl: "cts" };
 
-const bundle = ({ option }) => ({
+const bundleDecl = ({ option }) => ({
   input: "./src/index.ts",
-  exports: "named",
   output: [
     {
-      file: `./dist/${option.dir}/index.${option.extension}`,
+      file: `./types/index.d.${option.decl}`,
+      format: option.type,
+    },
+  ],
+  plugins: [dts({ tsconfig: `./tsconfig.${option.type}.json` })],
+});
+
+const bundleJS = ({ option }) => ({
+  input: "./src/index.ts",
+  output: [
+    {
+      file: `./dist/${option.type}/index.${option.extension}`,
       format: option.type,
     },
   ],
   plugins: [
     peerDeepsExternal(),
-    nodeResolve({ EXTENSIONS }),
+    nodeResolve({ extensions: EXTENSIONS }),
     commonjs(),
-    typescript(),
+    // Rollup은 모듈을 기반으로 빌드하므로 module: NodeNext 옵션을 사용할 수 없다.
+    typescript({ typescript: ttypescript }),
     json(),
     babel({
-      babelrcRoots: "../../",
+      exclude: "node_modules",
+      configFile: "../../babel.config.json",
       babelHelpers: "bundled",
-      include: `./src/**/*.(${EXTENSIONS.join("|")})`,
+      extensions: EXTENSIONS,
     }),
     terser({ format: { comments: false } }),
   ],
 });
 
-export const createBundler = () => ({
-  bundleESM: () => bundle({ option: ESM_OPTION }),
-  bundleCJS: () => bundle({ option: CJS_OPTION }),
-});
+export default [
+  bundleJS({ option: ESM_OPTION }),
+  bundleJS({ option: CJS_OPTION }),
+  bundleDecl({ option: ESM_OPTION }),
+  bundleDecl({ option: CJS_OPTION }),
+];
