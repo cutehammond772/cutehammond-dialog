411cd46494c5181daf89a2870d5bce26910dce1c
LEE\JHL <cutehammond772@gmail.com>
2023-03-23 17:56:58 +0900

    chore(core): index에 Resolver 관련 모듈 추가

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,10 @@ export { default as DialogArea } from "@/provider/components/DialogArea";
 
 /* resolver */
 export { default as DialogResolver } from "@/resolver/components/DialogResolver";
+export { useDefaultResolver } from "@/features/default/hooks";
+
+/* resolver utils */
+export * from "@/utils/resolver";
 
 /* type declarations */
 export type { DialogID, DialogKey, Dialog } from "$";
