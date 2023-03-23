78e46584ef6464869541c3e408d0feee946909a1
LEE\JHL <cutehammond772@gmail.com>
2023-03-23 13:17:56 +0900

    feat(core): index에 라이브러리 함수와 타입 정의 업데이트

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,11 @@
-export {};
+/* provider */
+export { default as DialogProvider } from "@/provider/components/DialogProvider";
+export { default as DialogArea } from "@/provider/components/DialogArea";
+
+/* resolver */
+export { useDialog } from "@/resolver/hooks";
+export { default as DialogResolver } from "@/resolver/components/DialogResolver";
+
+/* type declarations */
+export type { DialogID, DialogKey, Dialog } from "$";
+export type { DialogAreaProfile } from "$provider";
