8d6213a27de5f360df7bd4c5e2d7719a4c62287c
LEE\JHL <cutehammond772@gmail.com>
2023-03-27 17:45:31 +0900

    chore(core): 이후 처리해야 할 오류에 대한 주석 추가

--- packages/core/src/decl/features/event/common.ts
+++ packages/core/src/decl/features/event/common.ts
@@ -6,6 +6,7 @@ export type DOMEventPayload<T extends DOMEvent> = GlobalEventHandlersEventMap[T]
 
 export type DialogEventType = DialogEventID | DOMEvent;
 
+// Note: DOMEvent에 별도의 Payload를 주입 시 오류가 발생한다.
 export interface DialogEvent<T> {
   type: DialogEventType;
   payload: T;
